8b304a211373fe12b61fd663b8be735477bdf4aa
kyle <kylengelmann@gmail.com>
2020-09-20 12:56:06 -0500

    Add project

---
+++ BunnyBop/Assets/SpriteLightRendering/Runtime/Passes/ClearTargetsPass.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class ClearTargetsPass : ScriptableRenderPass
+{
+    string m_PassName;
+    ClearFlag m_clearFlag = ClearFlag.None;
+    Color m_clearColor = Color.black;
+
+    public ClearTargetsPass(string PassName, RenderPassEvent passEvent) : base()
+    {
+        renderPassEvent = passEvent;
+
+        m_PassName = PassName;
+    }
+
+    public void Setup(ClearFlag clearFlag, Color clearColor)
+    {
+        m_clearFlag = clearFlag;
+        m_clearColor = clearColor;
+    }
+
+    public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+    {
+        CommandBuffer commandBuffer = CommandBufferPool.Get(m_PassName);
+        commandBuffer.ClearRenderTarget((m_clearFlag & ClearFlag.Depth) != 0, (m_clearFlag & ClearFlag.Color) != 0, m_clearColor);
+        context.ExecuteCommandBuffer(commandBuffer);
+        CommandBufferPool.Release(commandBuffer);
+    }
+}
